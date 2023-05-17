ed9b26eeecc37cf4d1b71075613c5b4b65c83de4
Tony Chau <tonychau@Zazils-MBP.fios-router.home>
2023-05-16 22:11:28 -0400

    Added file-saver and react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Link, Route, Routes} from "react-router-dom";
+
+import { logo } from "./assets"
 
 const App = () => {
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <BrowserRouter>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+        <Link to="/">
+          <img src={logo} alt="logo" 
+          className="w-28 h-8 object-contain"/>
+        </Link>
+
+      </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[f9fafe] min-h-[calc(100vh-73px)]">
+        <Routes>
+          <Route path="/" element={""} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/assets/index.js
@@ -0,0 +1,5 @@
+import logo from "./intro_tony.jpg";
+
+export {
+    logo
+};
