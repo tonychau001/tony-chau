cd385a79ed680077d259229ad0600f9b3d1a15c0
Tony Chau <tonychau@Zazils-MBP.fios-router.home>
2023-05-24 17:11:08 -0400

    Added fading out to the nav bar. Added collapse navbar for smaller screens. Started on my expertise section.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,14 +6,45 @@ import { Header, Footer} from "./components";
 
 const App = () => {
   return (
-    <div style={{backgroundImage: `surl(${background})`}}>
+    <div className="bg-[#151019]">
       <Header />
-      <main className="sm:p-8 px-4 py-8 w-full  min-h-[calc(100vh-73px)]">
-        <div className="flex justify-center text-white text-[120px] pt-40">
+      <main style={{backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat'}} className="sm:p-8 px-4 py-8 w-full min-h-[calc(100vh-73px)]">
+        <div className="flex font-serif justify-center text-white text-[120px] pt-40">
           Tony Chau
         </div>
-        <div className="flex justify-center text-white text-[24px]">
+        <div className="flex font-serif justify-center text-white text-[24px]">
           SOFTWARE ENGINEER, FRONT END & BACK END DEVELOPER.
+        </div>
+
+        <div className="flex justify-center relative mt-96 text-white text-[105px]">
+          My Expertise
+        </div>
+
+        <div className="grid grid-rows-1 grid-flow-col border border-white">
+          <div className="border border-white text-white p-4">
+            Software Development, Java, SQL/NoSQL
+          </div>
+          <div className="border border-white text-white p-4">
+            FrontEnd Dev, ReactJS, TailWindCSS
+          </div>
+          <div className="border border-white text-white p-4">
+            BackEnd Dev, NodeJS, ExpressJS
+          </div>
+          {/* EXAMPLE */}
+          <div class="rounded overflow-hidden shadow-lg bg-white">
+          <img class="w-full" src="/mountain.jpg" alt="Mountain"/>
+          <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">Mountain</div>
+            <p class="text-gray-700 text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+          </div>
+          <div class="px-6 pt-4 pb-2">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+          </div>
+        </div>
         </div>
       </main>
     </div>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,27 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+    const [collapseButton, setCollapseButton] = useState(false);
+    const [navClassName, setNavClassName] = useState(["rounded-lg px-3 py-2 text-white font-medium font-mono",
+                                                            "rounded-lg px-3 py-2 text-white font-medium font-mono",
+                                                            "rounded-lg px-3 py-2 text-white font-medium font-mono",
+                                                            "rounded-lg px-3 py-2 text-white font-medium font-mono",
+                                                            "rounded-lg px-3 py-2 text-white font-medium font-mono"]);
+
+    const handleCollapseButton = () => {
+        setCollapseButton(prev => !prev);
+    }
+
+    const handleHoverNav = (e) => {
+        const id = e.target.id;
+
+        setNavClassName(prev => {
+            return prev.map((but, index) => {
+                if (index == id) {
+                    return "rounded-lg px-3 py-2 text-white font-medium font-mono";
+                } else {
+                    return "rounded-lg px-3 py-2 text-white font-medium font-mono opacity-50 transition-opacity";
+                }
+            });
+        });
+    }
+
+    const handleLeaveNav = () => {
+        setNavClassName(prev => {
+            return prev.map(() =>  "rounded-lg px-3 py-2 text-white font-medium font-mono opacity-100 transition-opacity");
+        });
+    }
+
     return (
-        <header className="w-full flex justify-between items-center sm:px-8 px-4 py-4">
+        <header className="w-full flex justify-between items-center sm:px-8 px-4 py-4 bg-[#151019]">    
             <div>
-                <div className="w-28 h-8 object-contain font-mono text-[#2DD4BF] text-[20px] hover:opacity-50">
+                <div className="w-28 h-8 object-contain font-mono font-bold text-[#6be4ff] text-[24px] hover:opacity-50 transition-opacity">
                 Tony_Chau
                 <div className="inline text-white">.</div>
-                <div className="inline text-[#E879F9]">dev</div>
+                <div className="inline text-[#FF55BB]">dev</div>
                 </div>
             </div>
 
-            <div className="flex sm:justify-center space-x-4 mx-auto">
+            <div className="invisible lg:visible flex sm:justify-center space-x-1 mx-auto">
             {[
-            ['Home', '/', 1],
-            ['About me', '/aboutme', 2],
-            ['Expertise', '/exoertise', 3],
-            ['Experience', '/Experience', 4],
-            ['Contact', '/contact', 5]
+            ['// Home', '/', 0],
+            ['// About me', '/aboutme', 1],
+            ['// Expertise', '/exoertise', 2],
+            ['// Experience', '/Experience', 3],
+            ['// Contact', '/contact', 4]
             ].map(([title, url, index]) => (
-            <a key={index} href={url} className="rounded-lg px-3 py-2 text-white font-small hover:bg-slate-100 hover:text-slate-900">{title}</a>
+            <a key={index} id={index} href={url} className={navClassName[index]} onMouseOver={handleHoverNav} onMouseLeave={handleLeaveNav}>{title}</a>
             ))}
             </div>
+            
+            <nav className="lg:hidden grid items-center sticky right-10 justify-items-end flex-wrap ">
+                <div className="block">
+                    <button className="flex items-center px-3 py-2 border rounded text-white border-white hover:opacity-50" onClick={handleCollapseButton}>
+                    <svg className="fill-current h-4 w-4" viewBox="0 0 20 20" ><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                    </button>
+                </div>
+               { collapseButton &&
+                    <div className="bg-white/5 rounded-lg w-[180px] pb-5 pt-2">
+                        <div className="text-sm grid justify-items-center">
+                        {[
+                                ['// Home', '/', 0],
+                                ['// About me', '/aboutme', 1],
+                                ['// Expertise', '/expertise', 2],
+                                ['// Experience', '/Experience', 3],
+                                ['// Contact', '/contact', 4]
+                                ].map(([title, url, index]) => (
+                                <a key={index} id={index} href={url} className="block mt-4 text-white hover:opacity-50 transition-opacity" onMouseOver={handleHoverNav} onMouseLeave={handleLeaveNav}>{title}</a>
+                                ))}
+                        </div>
+                    </div>}
+            </nav>      
+                        
         </header>
     );
 }
